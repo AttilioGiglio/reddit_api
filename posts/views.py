9eaae3023c868ac437dd6d0a02fe5f43c181ebd2
attiliogiglio <attilio.g93@gmail.com>
2020-12-04 20:45:03 -0300

    debugging on serializetion

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
+# generics.ListAPIView is going to list data very fast
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
