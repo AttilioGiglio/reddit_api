40a93b1b6fd2b9ec504b947fc807ea3114b2833f
attiliogiglio <attilio.g93@gmail.com>
2020-12-05 16:14:01 -0300

    api auth

--- posts/models.py
+++ posts/models.py
@@ -18,3 +18,6 @@ class Post(models.Model):
 class Vote(models.Model):
     voter = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.voter
